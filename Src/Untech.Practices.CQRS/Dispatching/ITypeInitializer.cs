af7e3dd038613e52d06230030dc9ef756857e084
Sergey Svirsky <zversergey@tut.by>
2017-08-22 15:57:59 +0300

    Name upd

--- Src/Untech.Practices.CQRS/Dispatching/ITypeInitializer.cs
+++ Src/Untech.Practices.CQRS/Dispatching/ITypeInitializer.cs
@@ -2,6 +2,11 @@ namespace Untech.Practices.CQRS.Dispatching
 {
 	public interface ITypeInitializer
 	{
-		void Init(object handler, object args);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="handler"></param>
+		/// <param name="request"></param>
+		void Init(object handler, object request);
 	}
 }
