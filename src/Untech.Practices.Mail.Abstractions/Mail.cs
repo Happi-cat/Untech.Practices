cdd7ff363b59f5ff300b4d05f3bf915ed0c62f3a
Svirski Siarhei <zversergey@tut.by>
2018-06-19 22:48:47 +0300

    Additional changes

--- src/Untech.Practices.Mail.Abstractions/Mail.cs
+++ src/Untech.Practices.Mail.Abstractions/Mail.cs
@@ -5,40 +5,68 @@ using System.Runtime.Serialization;
 
 namespace Untech.Practices.Mail
 {
+	/// <summary>
+	/// Represents mail information that can be used for rendering and sending.
+	/// </summary>
 	[DataContract]
-	public class Mail<TArgs>
+	public class Mail
 	{
+		/// <summary>
+		/// For serializers
+		/// </summary>
 		private Mail()
 		{
 		}
 
-		public Mail(MailboxAddress from, MailboxAddress to)
-			: this(from, new List<MailboxAddress> { to })
+		public Mail(string type, MailboxAddress to)
+			: this(type, new List<MailboxAddress> {to})
 		{
 		}
 
-		public Mail(MailboxAddress from, IEnumerable<MailboxAddress> to)
+		public Mail(string type, IEnumerable<MailboxAddress> to)
 		{
+			if (string.IsNullOrWhiteSpace(type)) throw new ArgumentNullException(nameof(type));
+
 			var toList = to?.ToList() ?? throw new ArgumentNullException(nameof(to));
 			if (toList.Count == 0) throw new ArgumentException(nameof(to));
 
-			From = from ?? throw new ArgumentNullException(nameof(from));
 			To = toList;
 		}
 
+		/// <summary>
+		/// Gets type of mail to process.
+		/// </summary>
 		[DataMember]
-		public MailboxAddress From { get; private set; }
+		public string Type { get; private set; }
 
+		/// <summary>
+		/// Gets or sets optional sender mail address.
+		/// </summary>
+		[DataMember]
+		public MailboxAddress From { get; set; }
+
+		/// <summary>
+		/// Gets recepients mail addresses.
+		/// </summary>
 		[DataMember]
 		public IReadOnlyCollection<MailboxAddress> To { get; private set; }
 
+		/// <summary>
+		/// Gets or sets optional CC mail adressess.
+		/// </summary>
 		[DataMember]
 		public IReadOnlyCollection<MailboxAddress> Cc { get; set; }
 
+		/// <summary>
+		/// Gets or sets optional BCC mail addresses.
+		/// </summary>
 		[DataMember]
 		public IReadOnlyCollection<MailboxAddress> Bcc { get; set; }
 
+		/// <summary>
+		/// Gets or sets additional mail arguments.
+		/// </summary>
 		[DataMember]
-		public TArgs Arguments { get; set; }
+		public object Arguments { get; set; }
 	}
 }
--- src/Untech.Practices.Mail.Abstractions/MailboxAddress.cs
+++ src/Untech.Practices.Mail.Abstractions/MailboxAddress.cs
@@ -3,13 +3,26 @@ using System.Runtime.Serialization;
 
 namespace Untech.Practices.Mail
 {
+	/// <summary>
+	/// Represents mail address.
+	/// </summary>
 	[DataContract]
 	public class MailboxAddress
 	{
+		/// <summary>
+		/// For serializers
+		/// </summary>
 		private MailboxAddress()
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of <see cref="MailboxAddress"/> with the specified <paramref name="email"/>.
+		/// </summary>
+		/// <param name="email">The email.</param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="email"/> is null or whitespace.
+		/// </exception>
 		public MailboxAddress(string email)
 		{
 			if (string.IsNullOrWhiteSpace(email)) throw new ArgumentNullException(nameof(email));
@@ -17,9 +30,15 @@ namespace Untech.Practices.Mail
 			Email = email;
 		}
 
+		/// <summary>
+		/// Gets email.
+		/// </summary>
 		[DataMember]
 		public string Email { get; private set; }
 
+		/// <summary>
+		/// Gets or sets display name if it was specified, otherwise returns null.
+		/// </summary>
 		[DataMember]
 		public string DisplayName { get; set; }
 
