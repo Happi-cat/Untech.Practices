f7464352165fc9d65d45d2bd91f4c544144e4785
Sergey Svirsky <svirski.siarhei@gmail.com>
2018-06-20 10:07:08 +0300

    Add Mail abstractions (#19)

---
+++ src/Untech.Practices.Mail.Abstractions/Mail.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Untech.Practices.Mail
+{
+	/// <summary>
+	/// Represents mail information that can be used for rendering and sending.
+	/// </summary>
+	[DataContract]
+	public class Mail
+	{
+		/// <summary>
+		/// For serializers
+		/// </summary>
+		private Mail()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new isntance of the <see cref="Mail"/> with a predefined recepient address and arguments.
+		/// </summary>
+		/// <param name="to">The recepient address.</param>
+		/// <param name="arguments">The mail arguments.</param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="to"/> or <paramref name="arguments"/> is null.
+		/// </exception>
+		public Mail(MailboxAddress to, IMailArguments arguments)
+		{
+			if (to == null) throw new ArgumentNullException(nameof(to));
+			if (arguments == null) throw new ArgumentNullException(nameof(arguments));
+
+			To = new List<MailboxAddress> { to };
+			Arguments = arguments;
+		}
+
+		/// <summary>
+		/// Initializes a new isntance of the <see cref="Mail"/> with a predefined recepients addresses and arguments.
+		/// </summary>
+		/// <param name="to">Recepients addresses.</param>
+		/// <param name="arguments">The mail arguments.</param>
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="to"/> or <paramref name="arguments"/> is null.
+		/// </exception>
+		/// <exception cref="ArgumentException"><paramref name="to"/> is empty.</exception>
+		public Mail(IEnumerable<MailboxAddress> to, IMailArguments arguments)
+		{
+			var toList = to?.ToList() ?? throw new ArgumentNullException(nameof(to));
+			if (toList.Count == 0) throw new ArgumentException(nameof(to));
+
+			To = toList;
+			Arguments = arguments;
+		}
+
+		/// <summary>
+		/// Gets type of mail to process.
+		/// </summary>
+		[DataMember]
+		public string Type { get; private set; }
+
+		/// <summary>
+		/// Gets or sets optional sender mail address.
+		/// </summary>
+		[DataMember]
+		public MailboxAddress From { get; set; }
+
+		/// <summary>
+		/// Gets recepients mail addresses.
+		/// </summary>
+		[DataMember]
+		public IReadOnlyCollection<MailboxAddress> To { get; private set; }
+
+		/// <summary>
+		/// Gets or sets optional CC mail adressess.
+		/// </summary>
+		[DataMember]
+		public IReadOnlyCollection<MailboxAddress> Cc { get; set; }
+
+		/// <summary>
+		/// Gets or sets optional BCC mail addresses.
+		/// </summary>
+		[DataMember]
+		public IReadOnlyCollection<MailboxAddress> Bcc { get; set; }
+
+		/// <summary>
+		/// Gets additional mail arguments.
+		/// </summary>
+		[DataMember]
+		public IMailArguments Arguments { get; private set; }
+	}
+}
