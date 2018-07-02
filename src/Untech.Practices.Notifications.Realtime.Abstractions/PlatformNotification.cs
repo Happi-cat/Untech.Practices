77fbd7d290e239c1348c5203042c03a357448896
Sergey Svirsky <zversergey@tut.by>
2018-07-02 13:32:39 +0300

    Add PlatformNotification

---
+++ src/Untech.Practices.Notifications.Realtime.Abstractions/PlatformNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Untech.Practices.Notifications.Realtime
+{
+	/// <summary>
+	/// Represents plaform-wide realtime notification.
+	/// </summary>
+	public class PlatformNotification : IRealtimeNotification
+	{
+		public PlatformNotification(object payload)
+		{
+			Payload = payload ?? throw new ArgumentNullException(nameof(payload));
+		}
+
+		public object Payload { get; }
+	}
+}
