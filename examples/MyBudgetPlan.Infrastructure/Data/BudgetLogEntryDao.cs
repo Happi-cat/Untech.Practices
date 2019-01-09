cf44d34cd253f595a4b8dc8dd5f67a2741088300
Sergey Svirsky <zversergey@tut.by>
2019-01-09 14:35:56 +0300

    squash! Add UserScopedGenericDataStorage for future use

--- examples/MyBudgetPlan.Infrastructure/Data/BudgetLogEntryDao.cs
+++ examples/MyBudgetPlan.Infrastructure/Data/BudgetLogEntryDao.cs
@@ -1,12 +1,14 @@
 using System;
+using MyBudgetPlan.Domain;
 using Shared.Infrastructure.Data;
 
 namespace MyBudgetPlan.Infrastructure.Data
 {
 	public abstract class BudgetLogEntryDao<T> : IUserScopedDao
+		where T: BudgetLogEntry
 	{
-		public DateTime When { get; set; }
 		public int Key { get; set; }
 		public int UserKey { get; set; }
+		public DateTime When { get; set; }
 	}
 }
