5e950a8db10f8466bfbbe797b250d18a3a9f5fb0
Sergey Svirsky <zversergey@tut.by>
2019-04-09 10:32:21 +0300

    add cqrs tests

--- test/Untech.AsyncCommandEngine.Tests/Processing/RequestProcessorTest.cs
+++ test/Untech.AsyncCommandEngine.Tests/Processing/RequestProcessorTest.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Untech.AsyncCommandEngine.Metadata;
+using Untech.Practices.CQRS;
+using Untech.Practices.CQRS.Dispatching;
 using Xunit;
 
 namespace Untech.AsyncCommandEngine.Processing
@@ -22,35 +25,141 @@ namespace Untech.AsyncCommandEngine.Processing
 		[Fact]
 		public async Task InvokeAsync_Returns_WhenFinalMiddlewareEnds()
 		{
-			var processor = new RequestProcessor(new []{ new FakeMiddleware() });
+			var processor = new RequestProcessor(new[]{ new FakeMiddleware() });
 			var context = new Context(new FakeRequest(), NullRequestMetadata.Instance);
 
 			await processor.InvokeAsync(context);
+		}
+
+		[Fact]
+		public async Task CqrsInvokeAsync_Returns_WhenCommandFound()
+		{
+			var processor = BuildProcessorForCqrsMiddlewareTest();
+			var context = BuildContextForCqrsMiddlewareTest(new FakeCommand());
+
+			await processor.InvokeAsync(context);
+		}
+
+		[Fact]
+		public async Task CqrsInvokeAsync_ThrowsInvalidOperation_WhenCommandNotFound()
+		{
+			var processor = BuildProcessorForCqrsMiddlewareTest();
+			var context = BuildContextForCqrsMiddlewareTest(new object());
+
+			await Assert.ThrowsAsync<InvalidOperationException>(() => processor.InvokeAsync(context));
+		}
+
+		[Fact]
+		public async Task CqrsInvokeAsync_ThrowsInvalidOperation_WhenCommandHasNoProperInterfaces()
+		{
+			var processor = BuildProcessorForCqrsMiddlewareTest();
+			var context = BuildContextForCqrsMiddlewareTest(new FakeCommandWithoutInterface());
+
+			await Assert.ThrowsAsync<InvalidOperationException>(() => processor.InvokeAsync(context));
+		}
+
+		[Fact]
+		public async Task CqrsInvokeAsync_ThrowsInvalidOperation_WhenCommandHasMultipleInterfaces()
+		{
+			var processor = BuildProcessorForCqrsMiddlewareTest();
+			var context = BuildContextForCqrsMiddlewareTest(new FakeCommandWithMultipleInterfaces());
+
+			await Assert.ThrowsAsync<InvalidOperationException>(() => processor.InvokeAsync(context));
+		}
+
+		private static IRequestProcessor BuildProcessorForCqrsMiddlewareTest()
+		{
+			return new EngineBuilder().BuildProcessor(new FakeCqrsStrategy());
+		}
+
+		private static Context BuildContextForCqrsMiddlewareTest(object command)
+		{
+			return new Context(new FakeRequest(command), NullRequestMetadata.Instance);
 		}
 
 		private class FakeMiddleware : IRequestProcessorMiddleware
 		{
 			public Task InvokeAsync(Context context, RequestProcessorCallback next)
 			{
-				return Task.FromResult(0);
+				return Task.CompletedTask;
+			}
+		}
+
+		private class FakeCqrsStrategy : ICqrsStrategy, IDispatcher
+		{
+			private readonly IReadOnlyList<Type> _types = new List<Type>
+			{
+				typeof(FakeCommand), typeof(FakeCommandWithoutInterface), typeof(FakeCommandWithMultipleInterfaces)
+			};
+
+			public Type FindRequestType(string requestName)
+			{
+				return _types.FirstOrDefault(t => t.FullName == requestName);
+			}
+
+			public IDispatcher GetDispatcher(Context context)
+			{
+				return this;
+			}
+
+			public Task<TResult> FetchAsync<TResult>(IQuery<TResult> query, CancellationToken cancellationToken)
+			{
+				return Task.FromResult(default(TResult));
+			}
+
+			public Task PublishAsync(INotification notification, CancellationToken cancellationToken)
+			{
+				return Task.CompletedTask;
+			}
+
+			public Task<TResult> ProcessAsync<TResult>(ICommand<TResult> command, CancellationToken cancellationToken)
+			{
+				return Task.FromResult(default(TResult));
 			}
 		}
 
 		private class FakeRequest : Request
 		{
+			private readonly object _body;
+
+			public FakeRequest() : this(new FakeCommand())
+			{
+			}
+
+			public FakeRequest(object body)
+			{
+				Identifier = Guid.NewGuid().ToString();
+				Name = body.GetType().FullName;
+				Created = DateTimeOffset.Now;
+				_body = body;
+			}
+
 			public override string Identifier { get; }
 			public override string Name { get; }
 			public override DateTimeOffset Created { get; }
 			public override IDictionary<string, string> Attributes { get; }
+
 			public override object GetBody(Type requestType)
 			{
-				throw new NotImplementedException();
+				return _body;
 			}
 
 			public override Stream GetRawBody()
 			{
 				throw new NotImplementedException();
 			}
+		}
+
+		private class FakeCommand : ICommand
+		{
+		}
+
+		private class FakeCommandWithoutInterface
+		{
+		}
+
+		private class FakeCommandWithMultipleInterfaces : ICommand<int>, ICommand<string>
+		{
 		}
 	}
 }
