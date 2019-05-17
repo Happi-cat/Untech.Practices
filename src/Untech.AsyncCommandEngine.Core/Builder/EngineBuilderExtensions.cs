9452e189cce32a812634eaca62d83a16f8e62a6a
Sergey Svirsky <zversergey@tut.by>
2019-05-17 16:16:13 +0300

    add MiddlewareCollectionExtensions

--- src/Untech.AsyncCommandEngine.Core/Builder/EngineBuilderExtensions.cs
+++ src/Untech.AsyncCommandEngine.Core/Builder/EngineBuilderExtensions.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Untech.AsyncCommandEngine.Metadata;
-using Untech.AsyncCommandEngine.Processing;
 using Untech.AsyncCommandEngine.Transports;
 
 namespace Untech.AsyncCommandEngine.Builder
@@ -88,51 +86,6 @@ namespace Untech.AsyncCommandEngine.Builder
 			if (providers == null) throw new ArgumentNullException(nameof(providers));
 
 			return builder.ReadMetadataFrom(new CompositeRequestMetadataProvider(providers));
-		}
-
-		/// <summary>
-		/// Registers middleware.
-		/// </summary>
-		/// <param name="builder">The builder to use for middleware registration.</param>
-		/// <param name="middleware">The instance of the <see cref="IRequestProcessorMiddleware"/>.</param>
-		/// <returns></returns>
-		public static MiddlewareCollection Then(this MiddlewareCollection builder,
-			IRequestProcessorMiddleware middleware)
-		{
-			if (builder == null) throw new ArgumentNullException(nameof(builder));
-			if (middleware == null) throw new ArgumentNullException(nameof(middleware));
-
-			return builder.Then(ctx => middleware);
-		}
-
-		/// <summary>
-		/// Registers middleware.
-		/// </summary>
-		/// <param name="builder">The builder to use for middleware registration.</param>
-		/// <param name="middleware">The <see cref="IRequestProcessor"/> middleware.</param>
-		/// <returns></returns>
-		public static MiddlewareCollection Then(this MiddlewareCollection builder,
-			Func<Context, RequestProcessorCallback, Task> middleware)
-		{
-			if (builder == null) throw new ArgumentNullException(nameof(builder));
-			if (middleware == null) throw new ArgumentNullException(nameof(middleware));
-
-			return builder.Then(ctx => new AdHocRequestProcessorMiddleware(middleware));
-		}
-
-		/// <summary>
-		/// Registers middleware.
-		/// </summary>
-		/// <param name="builder">The builder to use for middleware registration.</param>
-		/// <param name="creator">The function that creates <see cref="IRequestProcessorMiddleware"/>.</param>
-		/// <returns></returns>
-		public static MiddlewareCollection Then(this MiddlewareCollection builder,
-			Func<IBuilderContext, Func<Context, RequestProcessorCallback, Task>> creator)
-		{
-			if (builder == null) throw new ArgumentNullException(nameof(builder));
-			if (creator == null) throw new ArgumentNullException(nameof(creator));
-
-			return builder.Then(ctx => new AdHocRequestProcessorMiddleware(creator(ctx)));
 		}
 	}
 }
