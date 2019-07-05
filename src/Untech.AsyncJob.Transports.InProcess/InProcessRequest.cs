9bb0a02e0dc15fab79c8cd2b7b3e492a1831b7fa
Sergey Svirsky <zversergey@tut.by>
2019-07-05 15:30:12 +0300

    add InProcess transport

---
+++ src/Untech.AsyncJob.Transports.InProcess/InProcessRequest.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using Untech.Practices.CQRS.Dispatching;
+
+namespace Untech.AsyncJob.Transports.InProcess
+{
+	internal class InProcessRequest:  Request
+	{
+		private readonly string _payload;
+
+		public InProcessRequest(object payload, QueueOptions options)
+		{
+			Identifier = Guid.NewGuid().ToString("B");
+			Name = payload.GetType().FullName;
+			Created = DateTimeOffset.Now;
+			_payload = JsonSerializer.ToString(payload);
+
+			QueueOptions = options;
+			Attributes = options?.Advanced?.ToDictionary(n => n.Key, n => Convert.ToString((object)n.Value));
+		}
+
+		public override string Identifier { get; }
+		public override string Name { get; }
+		public override DateTimeOffset Created { get; }
+
+		public override IDictionary<string, string> Attributes { get; }
+
+		public QueueOptions QueueOptions { get; }
+
+		public int GetPriority()
+		{
+			return QueueOptions?.Priority ?? 0;
+		}
+
+		public bool IsExpired()
+		{
+			return QueueOptions?.ExpiresAfter != null
+				&& Created + QueueOptions.ExpiresAfter <= DateTimeOffset.Now;
+		}
+
+		public bool IsExecuteAfterReached()
+		{
+			return QueueOptions?.ExecuteAfter == null
+				|| Created + QueueOptions.ExecuteAfter <= DateTimeOffset.Now;
+		}
+
+		public override object GetBody(Type requestType)
+		{
+			return JsonSerializer.Parse(_payload, requestType);
+		}
+
+		public override Stream GetRawBody()
+		{
+			return new MemoryStream(Encoding.UTF8.GetBytes(_payload));
+		}
+	}
+}
---
+++ src/Untech.AsyncJob.Transports.InProcess/InProcessTransport.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Untech.Practices.CQRS;
+using Untech.Practices.CQRS.Dispatching;
+
+namespace Untech.AsyncJob.Transports.InProcess
+{
+	public class InProcessTransport : ITransport, IQueueDispatcher
+	{
+		private readonly IDictionary<int, Queue> _queues;
+		public InProcessTransport()
+		{
+			_queues = new Dictionary<int, Queue>(new PriorityComparer(10))
+			{
+				{ -1, new Queue() }, { 0, new Queue() }, { 1, new Queue() }
+			};
+		}
+
+		public Task<ReadOnlyCollection<Request>> GetRequestsAsync(int count)
+		{
+			var maxCountPerPriorityQueue = Math.Max(count - 2, 1);
+			var requests = _queues
+				.OrderByDescending(n => n.Key)
+				.SelectMany(n => n.Value.Dequeue(maxCountPerPriorityQueue))
+				.Take(count)
+				.OrderByDescending(r => r.GetPriority())
+				.ToList<Request>()
+				.AsReadOnly();
+
+			return Task.FromResult(requests);
+		}
+
+		public async Task CompleteRequestAsync(Request request)
+		{
+		}
+
+		public async Task FailRequestAsync(Request request, Exception exception)
+		{
+		}
+
+		public Task EnqueueAsync<TResult>(ICommand<TResult> command, CancellationToken cancellationToken = default,
+			QueueOptions options = null)
+		{
+			var request = new InProcessRequest(command, options);
+			_queues[request.GetPriority()].Enqueue(request);
+			return Task.CompletedTask;
+		}
+
+		public Task EnqueueAsync(INotification notification, CancellationToken cancellationToken = default,
+			QueueOptions options = null)
+		{
+			var request = new InProcessRequest(notification, options);
+			_queues[request.GetPriority()].Enqueue(request);
+			return Task.CompletedTask;
+		}
+
+		/// <summary>
+		/// Assumes 3 priority groups: -1, 0, 1. Divides numbers into groups according to scale factor.
+		/// </summary>
+		private class PriorityComparer : IEqualityComparer<int>
+		{
+			private readonly int _factor;
+
+			public PriorityComparer(int scaleFactor = 1)
+			{
+				_factor = Math.Max(scaleFactor, 1);
+			}
+
+			public bool Equals(int x, int y)
+			{
+				return Normalize(x) == Normalize(y);
+			}
+
+			public int GetHashCode(int obj)
+			{
+				return Normalize(obj).GetHashCode();
+			}
+
+			private int Normalize(int value)
+			{
+				value /= _factor;
+				value = Math.Max(value, -1);
+				return Math.Min(value, 1);
+			}
+		}
+
+		private class Queue
+		{
+			private readonly ConcurrentQueue<InProcessRequest> _queue;
+			private readonly ConcurrentQueue<InProcessRequest> _delayQueue;
+
+			public Queue()
+			{
+				_queue = new ConcurrentQueue<InProcessRequest>();
+				_delayQueue = new ConcurrentQueue<InProcessRequest>();
+			}
+
+			public IEnumerable<InProcessRequest> Dequeue(int count)
+			{
+				return Delayed(count).Concat(Main(count)).Take(count);
+			}
+
+			public void Enqueue(InProcessRequest item)
+			{
+				_queue.Enqueue(item);
+			}
+
+			private IEnumerable<InProcessRequest> Delayed(int count)
+			{
+				count = Math.Max(count / 2, 1);
+				return TryDeque(_delayQueue, _delayQueue, count);
+			}
+
+			private IEnumerable<InProcessRequest> Main(int count)
+			{
+				return TryDeque(_queue, _delayQueue, count * 2);
+			}
+
+			private static IEnumerable<InProcessRequest> TryDeque(
+				ConcurrentQueue<InProcessRequest> queue,
+				ConcurrentQueue<InProcessRequest> delayQueue,
+				int maxAttempts)
+			{
+				while (maxAttempts-- > 0)
+				{
+					if (!queue.TryDequeue(out var request)) break;
+
+					if (request.IsExpired()) continue;
+					if (request.IsExecuteAfterReached())
+					{
+						yield return request;
+					}
+					else
+					{
+						delayQueue.Enqueue(request);
+					}
+				}
+			}
+
+		}
+	}
+}
