6da934ade883760e52e3dca76242599d5162113c
Sergey Svirsky <zversergey@tut.by>
2019-08-12 10:34:55 +0300

    Drop sync method in distributed lock (#42)
    * Drop sync version
    * Drop DistributedLockTimeoutException
    * Rename DistributedLockException into DistributedLockNotAcquiredException
    * Use cancellationToken for waitTime in DistributedLockManager

--- src/Untech.Practices.Concurrency.Abstractions/IDistributedLockManager.cs
+++ src/Untech.Practices.Concurrency.Abstractions/IDistributedLockManager.cs
@@ -10,34 +10,15 @@ namespace Untech.Practices.Concurrency
 	public interface IDistributedLockManager
 	{
 		/// <summary>
-		///     Acquires distributed lock for the specified <paramref name="resource" /> or throws exception.
-		/// </summary>
-		/// <param name="resource">Resource name to lock.</param>
-		/// <param name="options">Lock options to use.</param>
-		/// <returns>Disposable lock.</returns>
-		/// <exception cref="TimeoutException">Acquiring timeouted.</exception>
-		/// <exception cref="DistributedLockException">Lock was not acquired.</exception>
-		IDisposable Acquire(string resource, LockOptions options);
-
-		/// <summary>
 		///     Acquires distributed lock asynchronously for the specified <paramref name="resource" /> or throws exception.
 		/// </summary>
 		/// <param name="resource">Resource name to lock.</param>
 		/// <param name="options">Lock options to use.</param>
 		/// <param name="cancellationToken">Cancellation token.</param>
 		/// <returns>Disposable lock.</returns>
-		/// <exception cref="TimeoutException">Acquiring timeouted.</exception>
-		/// <exception cref="DistributedLockException">Lock was not acquired.</exception>
+		/// <exception cref="DistributedLockNotAcquiredException">Lock was not acquired.</exception>
 		Task<IDisposable> AcquireAsync(string resource, LockOptions options,
 			CancellationToken cancellationToken = default);
-
-		/// <summary>
-		///     Tries to acquire distributed lock for the specified <paramref name="resource" />.
-		/// </summary>
-		/// <param name="resource">Resource name to lock.</param>
-		/// <param name="options">Lock options to use.</param>
-		/// <returns>Disposable lock if was acquired; otherwise null.</returns>
-		IDisposable TryAcquire(string resource, LockOptions options);
 
 		/// <summary>
 		///     Tries to acquire distributed lock asynchronously for the specified <paramref name="resource" /> or throws
