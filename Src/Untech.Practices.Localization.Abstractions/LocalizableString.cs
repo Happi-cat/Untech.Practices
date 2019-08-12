8a9392ddf67ac892d5a7f2200b6d4e6792cb5fad
Sergey Svirsky <zversergey@tut.by>
2019-08-12 10:44:03 +0300

    Added localization (#8)

---
+++ Src/Untech.Practices.Localization.Abstractions/LocalizableString.cs
@@ -0,0 +1,25 @@
+﻿namespace Untech.Practices.Localization
+{
+	public struct LocalizableString : ILocalizableString
+	{
+		public LocalizableString(string reference, string source)
+		{
+			Reference = reference;
+			Source = source;
+		}
+
+		public string Reference { get; }
+		public string Source { get; }
+
+		public string Localize(ILocalizationContext context)
+		{
+			return context.GetSource(Source).GetString(Reference);
+		}
+
+		public string Localize(ILocalizationContext context, params object[] args)
+		{
+			var localizedString = Localize(context);
+			return string.Format(localizedString, args);
+		}
+	}
+}
