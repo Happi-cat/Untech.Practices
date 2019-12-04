161adddaa5216981d640da86b4b088f637d7a73a
Sergey Svirsky <zversergey@tut.by>
2019-12-04 10:31:24 +0300

    minor change in Localizations

--- examples/Localization/Localizations.cs
+++ examples/Localization/Localizations.cs
@@ -10,24 +10,26 @@ namespace Localization
 		{
 			return new LocalizationSource(new[]
 			{
-				new LocalizationPartition("web", CultureInfo.InvariantCulture,
-					new Dictionary<string, string>
-					{
-						{ "home", "Home" },
-						{ "home:description", "Go to home page" },
-						{ "contacts", "Contacts" },
-						{ "contacts:description", "See out contacts" },
-						{ "about", "About Us" },
-						{ "about:description", "Read more info about us" },
-					}),
-				new LocalizationPartition("web", new CultureInfo("ru-RU"),
-					new Dictionary<string, string>
-					{
-						{ "home:description", "Перейти на домашнюю страницу" },
-						{ "contacts:description", "Контактная информация" },
-						{ "about:description", "Кто мы" },
-					})
+				new LocalizationPartition("web", CultureInfo.InvariantCulture, GetDefault()),
+				new LocalizationPartition("web", new CultureInfo("ru-RU"), GetRussian())
 			});
+
+			Dictionary<string, string> GetDefault() => new Dictionary<string, string>
+			{
+				{ "home", "Home" },
+				{ "home:description", "Go to home page" },
+				{ "contacts", "Contacts" },
+				{ "contacts:description", "See out contacts" },
+				{ "about", "About Us" },
+				{ "about:description", "Read more info about us" },
+			};
+
+			Dictionary<string, string> GetRussian() => new Dictionary<string, string>
+			{
+				{ "home:description", "Перейти на домашнюю страницу" },
+				{ "contacts:description", "Контактная информация" },
+				{ "about:description", "Кто мы" },
+			};
 		}
 	}
 }
