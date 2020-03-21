980d19beb56057876edef57dbe9c25beca235c53
Sergey Svirsky <zversergey@tut.by>
2020-03-21 16:21:16 +0300

    upd RequestProcessor to support endware

--- src/Untech.AsyncJob.Abstractions/Processing/RequestProcessor.cs
+++ src/Untech.AsyncJob.Abstractions/Processing/RequestProcessor.cs
@@ -1,45 +1,54 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Untech.AsyncJob.Processing
 {
-	internal class RequestProcessor : IRequestProcessor
+	public class RequestProcessor : IRequestProcessor
 	{
 		private readonly RequestProcessorCallback _next;
 
-		public RequestProcessor(IEnumerable<IRequestProcessorMiddleware> steps)
+		public RequestProcessor(IEnumerable<IRequestProcessorMiddleware> middlewares)
+			: this(middlewares, new NotHandledRequestProcessor())
 		{
-			if (steps == null)
-				throw new ArgumentNullException(nameof(steps));
+		}
 
-			_next = BuildChain(new Queue<IRequestProcessorMiddleware>(steps));
+		public RequestProcessor(IEnumerable<IRequestProcessorMiddleware> middlewares, IRequestProcessor endware)
+		{
+			if (middlewares == null) throw new ArgumentNullException(nameof(middlewares));
+			if (endware == null) throw new ArgumentNullException(nameof(endware));
+
+			_next = BuildChain(new Queue<IRequestProcessorMiddleware>(middlewares), endware);
 		}
 
 		public Task InvokeAsync(Context context)
 		{
-			if (context == null)
-				throw new ArgumentNullException(nameof(context));
+			if (context == null) throw new ArgumentNullException(nameof(context));
 
 			return _next(context);
 		}
 
-		private static RequestProcessorCallback BuildChain(Queue<IRequestProcessorMiddleware> steps)
+		private static RequestProcessorCallback BuildChain(
+			Queue<IRequestProcessorMiddleware> middlewares,
+			IRequestProcessor endware)
 		{
-			if (steps.Count <= 0)
-				return DoFinally;
+			if (middlewares.Count <= 0) return endware.InvokeAsync;
 
-			var currentMiddleware = steps.Dequeue();
-			var next = BuildChain(steps);
+			var currentMiddleware = middlewares.Dequeue();
+			var next = BuildChain(middlewares, endware);
 
 			return currentMiddleware != null
 				? ctx => currentMiddleware.InvokeAsync(ctx, next)
 				: next;
 		}
 
-		private static Task DoFinally(Context ctx)
+		private class NotHandledRequestProcessor : IRequestProcessor
 		{
-			throw new InvalidOperationException("Request wasn't handled in previous middleware.");
+			public Task InvokeAsync(Context context)
+			{
+				throw new InvalidOperationException("Request wasn't handled in previous middleware.");
+			}
 		}
 	}
 }
